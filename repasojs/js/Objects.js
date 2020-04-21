dbaa9604379fb8d8123c208d2895d738c5b5d334
giojimen3z <gioud@outlook.com>
2020-04-21 17:45:54 -0500

    test

--- repasojs/js/Objects.js
+++ repasojs/js/Objects.js
@@ -39,3 +39,15 @@ console.log(tarea1);
 const mostrarInfo = mostrarInformacionTarea(tarea1.nombre, tarea1.urgencia)
 console.log(mostrarInfo);
 console.log(tarea1.mostrarInformacionTarea())
+
+//Destructuring objects
+// Destructuring es extraer la informaciond e  los objetos
+
+const aprendiendoJS = {
+
+    version: 'ES6+',
+    frameworks: ['React','Vuejs','AngularJs']
+
+}
+
+console.log(aprendiendoJS);
